316a451d2491bbe9823b853240f8b206925afe44
tywangcharlie <twang220@uic.edu>
2020-05-24 20:20:06 -0500

    gh-pages added

---
+++ precache-manifest.a76d99d13841156619cc803dbeb30330.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "289e9b9f248f829d808a56dd957a1e04",
+    "url": "/portfoliowebsite/index.html"
+  },
+  {
+    "revision": "0ea1d453ea711453c203",
+    "url": "/portfoliowebsite/static/css/2.7c388b1d.chunk.css"
+  },
+  {
+    "revision": "bb32b93215e586292e74",
+    "url": "/portfoliowebsite/static/css/main.1116cb17.chunk.css"
+  },
+  {
+    "revision": "0ea1d453ea711453c203",
+    "url": "/portfoliowebsite/static/js/2.3b540c3d.chunk.js"
+  },
+  {
+    "revision": "bb32b93215e586292e74",
+    "url": "/portfoliowebsite/static/js/main.d3c3066b.chunk.js"
+  },
+  {
+    "revision": "9b7897b2239eb5cc8e4e",
+    "url": "/portfoliowebsite/static/js/runtime~main.ca92c413.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/portfoliowebsite/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/portfoliowebsite/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/portfoliowebsite/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/portfoliowebsite/static/media/slick.f97e3bbf.svg"
+  }
+]);
